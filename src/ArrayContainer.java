f28c4dda45710bbd7f084cde6736fb456cf69179
bart0709 <bart0709@stud.kea.dk>
2021-11-28 00:21:00 +0100

    added more functionality to the class

--- src/ArrayContainer.java
+++ src/ArrayContainer.java
@@ -3,17 +3,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class ArrayContainer {
-	private static final HashMap<Class<?>, ArrayList<?>> db = new HashMap<>();
+	private final HashMap<Class<?>, ArrayList<?>> db = new HashMap<>();
 
-	public static <T> void addArrayList(Class<T> type, ArrayList<T> list) {
+	public <T> void putArrayList(Class<T> type, ArrayList<T> list) {
 		if (type == null) {
 			throw new NullPointerException("Type is null");
 		}
 		db.put(type, list);
 	}
 
+	public <T> void removeArrayList(Class<T> type) {
+		if (type == null) {
+			throw new NullPointerException("Type is null");
+		}
+		db.remove(type);
+	}
+
 	@SuppressWarnings("unchecked")
-	public static <T> ArrayList<T> getArrayList(Class<T> type) {
+	public <T> ArrayList<T> getArrayList(Class<T> type) {
 		return (ArrayList<T>) db.get(type);
 	}
+
+	public <T> boolean containsClass(Class<T> type) {
+		return db.containsKey(type);
+	}
+
 }
