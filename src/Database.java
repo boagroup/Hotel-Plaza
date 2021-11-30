c8648c1249dec5d1147b4645ff4b3a71d59b5610
bart0709 <bart0709@stud.kea.dk>
2021-11-30 21:17:26 +0100

    Database class created, saveDatabase() defined

---
+++ src/Database.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public final class Database implements Serializable {
+	private static ArrayContainer db = new ArrayContainer();
+	private static String test = "test";
+	private static final String DIR = System.getProperty("user.dir") + File.separator + "database";
+
+	private Database() {
+		throw new RuntimeException("Instantation of Database is not allowed");
+	}
+
+	public static boolean saveDatabase() {
+		File folder = new File(DIR);
+		try {
+			if (!folder.mkdir() && !folder.exists()) {
+				throw new IOException("Creating or finding folder failed");
+			}
+			FileOutputStream fos = new FileOutputStream(DIR + File.separator + "db");
+			ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.writeObject(db);
+			out.close();
+			fos.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean loadDatabase() {
+		return true;
+	}
+
+	public static <T> ArrayList<T> getList(Class<T> type) {
+		return db.getArrayList(type);
+	}
+
+	public static void main(String[] args) {
+		if (Database.saveDatabase()) {
+			System.out.println("TRUE");
+		} else {
+			System.out.println("FALSE");
+		}
+	}
+
+}
