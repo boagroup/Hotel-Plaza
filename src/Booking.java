c8dda34ad1caa923d3341f262f4cc9af06fb55e8
Octavian-Roman <work.octavian@gmail.com>
2021-11-30 22:54:55 +0100

    Adding 'staff' and 'booking' classes

---
+++ src/Booking.java
@@ -0,0 +1,111 @@
+import java.util.Date;
+
+public class Booking extends Item {
+    protected Room room;
+    protected Guest guest;
+    protected Date checkInDate;
+    protected Date checkOutDate;
+    protected int stayPeriod; // number of days
+    protected double cost; // total amount in DKK
+
+    public Booking(Room room, Guest guest) {
+        this.room = room;
+        this.guest = guest;
+        generateTags();
+    }
+
+    public Booking(Room room, Guest guest, Date checkInDate) {
+        this.room = room;
+        this.guest = guest;
+        this.checkInDate = checkInDate;
+        generateTags();
+    }
+
+    public Booking(Room room, Guest guest, Date checkInDate, Date checkOutDate, int stayPeriod, double cost) {
+        this.room = room;
+        this.guest = guest;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.stayPeriod = stayPeriod;
+        this.cost = cost;
+        generateTags();
+    }
+
+    public boolean search() {
+        return false;
+    }
+
+    public void generateTags() {
+        this.tag =
+                room + " "
+                + guest + " "
+                + checkInDate + " "
+                + checkOutDate + " "
+                + stayPeriod + " "
+                + cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "room=" + room +
+                ", guest=" + guest +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", stayPeriod=" + stayPeriod +
+                ", cost=" + cost +
+                '}';
+    }
+
+    // Getters
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public int getStayPeriod() {
+        return stayPeriod;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    // Setters
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public void setStayPeriod(int stayPeriod) {
+        this.stayPeriod = stayPeriod;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
---
+++ src/Staff.java
@@ -0,0 +1,97 @@
+public class Staff extends Item {
+    protected String name;
+    protected String position;
+    protected double salary; // Hourly base salary in DKK
+    protected String gender;
+    protected int age;
+
+    public Staff(String name) {
+        this.name = name;
+        generateTags();
+    }
+
+    public Staff(String name, String position) {
+        this.name = name;
+        this.position = position;
+        generateTags();
+    }
+
+    public Staff(String name, String position, double salary, String gender, int age) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.gender = gender;
+        this.age = age;
+        generateTags();
+    }
+
+    public boolean search() {
+        return false;
+    }
+
+    public void generateTags() {
+        this.tag =
+                name + " "
+                + position + " "
+                + salary + " "
+                + gender + " "
+                + age;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", salary=" + salary +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    // Getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
