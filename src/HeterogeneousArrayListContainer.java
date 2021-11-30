97163c0ccd7a34c349f059d538abed7070e212f2
bart0709 <bart0709@stud.kea.dk>
2021-12-01 00:23:50 +0100

    added constructors plus early documentantion

--- src/HeterogeneousArrayListContainer.java
+++ src/HeterogeneousArrayListContainer.java
@@ -2,10 +2,27 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * stores hashmap with Classes as keys an Arraylist of instances of those classes as values
+ */
 public class HeterogeneousArrayListContainer implements Serializable {
-	private final HashMap<Class<?>, ArrayList<?>> hM = new HashMap<>();
+	private final HashMap<Class<?>, ArrayList<?>> hM;
 
-	public HeterogeneousArrayListContainer() {}
+	/**
+	 * constructs an empty HeterogeneousArrayListContainer with the default initial capacity of 16 (same as Hashmap)
+	 */
+	public HeterogeneousArrayListContainer() {
+		hM = new HashMap<>();
+	}
+
+	/**
+	 * constructs an empty HeterogeneousArrayListContainer with the default initial capacity of 16 (same as Hashmap)
+	 * @param  initialCapacity the initial capacity.
+	 * @throws IllegalArgumentException if the initial capacity is negative.
+	 */
+	public HeterogeneousArrayListContainer(int initialCapacity) {
+		hM = new HashMap<>(initialCapacity);
+	}
 
 	public <T> void putArrayList(Class<T> type, ArrayList<T> list) throws NullPointerException {
 		if (type == null) {
