3a2251b51da4618fb37f9b5179568e3e0fe9d8ed
bart0709 <bart0709@stud.kea.dk>
2021-11-30 19:43:29 +0100

    changed ArrayContainer into non-abstract class

--- src/ArrayContainer.java
+++ src/ArrayContainer.java
@@ -1,31 +1,39 @@
-import interfaces.iDatabase;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class ArrayContainer {
-	private final HashMap<Class<?>, ArrayList<?>> db = new HashMap<>();
+public class ArrayContainer implements Serializable {
+	private final HashMap<Class<?>, ArrayList<?>> hM = new HashMap<>();
+
+	public ArrayContainer() {}
 
 	public <T> void putArrayList(Class<T> type, ArrayList<T> list) {
 		if (type == null) {
 			throw new NullPointerException("Type is null");
 		}
-		db.put(type, list);
+		hM.put(type, list);
 	}
 
 	public <T> void removeArrayList(Class<T> type) {
 		if (type == null) {
 			throw new NullPointerException("Type is null");
 		}
-		db.remove(type);
+		hM.remove(type);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> ArrayList<T> getArrayList(Class<T> type) {
-		return (ArrayList<T>) db.get(type);
+		if (type == null) {
+			throw new NullPointerException("Type is null");
+		}
+		return (ArrayList<T>) hM.get(type);
 	}
 
 	public <T> boolean containsClass(Class<T> type) {
-		return db.containsKey(type);
+		if (type == null) {
+			throw new NullPointerException("Type is null");
+		}
+		return hM.containsKey(type);
 	}
 
 }
